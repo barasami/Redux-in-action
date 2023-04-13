316a486493be301b5ce80dbd092aa8a8f5d7f265
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-13 22:13:36 +0300

    modified pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form'>
+        <div>
+          <input type='number' placeholder='Enter numb1'></input>
+        </div>
+        <div>
+          <input type='number' placeholder='Enter numb2'></input>
+        </div>
+        <div>
+          <button>Add</button>
+          <button>Subtract</button>
+          <button>Multiply</button>
+        </div>
+      </form>
     </div>
   );
 }
